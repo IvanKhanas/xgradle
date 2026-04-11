f92e8fb52d38a05f841c0e2900abb1551db8cc21
Ivan Khanas <xeno@altlinux.org>
2026-04-11 23:29:48 +0300

    Replace cycles with stream api

--- xgradle-resolution-plugin/src/test/java/unittests/logging/DependencyLoggerTests.java
+++ xgradle-resolution-plugin/src/test/java/unittests/logging/DependencyLoggerTests.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
--- xgradle-resolution-plugin/src/test/java/unittests/services/ArtifactVerifierTests.java
+++ xgradle-resolution-plugin/src/test/java/unittests/services/ArtifactVerifierTests.java
@@ -31,7 +31,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
